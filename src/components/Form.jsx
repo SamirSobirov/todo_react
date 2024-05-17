34ffa37607e77cc21ac43460d82d55d0b973dd4b
Samir <sobiroff23@gmail.com>
2024-05-17 14:35:47 +0500

    fixed

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -54,7 +54,7 @@ export default function TodoList() {
             <hr className='mt-4 ' />
             <div className="container px-[20px] pt-8 pr-[50px] grid grid-cols-3 gap-20 ">
                 {todos.length === 0 ?
-                    <img src="./public/nothing_img.png" alt="image" className="h-[350px] w-full ml-[115%]" />
+                    <img src="/images/nothing_img.png" alt="image" className="h-[350px] w-full ml-[115%]" />
                     :
                     todos.map(todo => (
                         <div  key={todo.id} className= {`box ${todo.completed ? 'completed' : ' border-[2px] rounded-xl  border-black'}`}>
